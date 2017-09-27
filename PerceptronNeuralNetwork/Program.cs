13fb1d300361c6907754636b2da305d0130b3300
Philip Doxakis <doxakis@users.noreply.github.com>
2017-09-27 19:41:56 -0400

    Add project files.

---
+++ PerceptronNeuralNetwork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronNeuralNetwork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
