8028745979476e5cf6030b6bf8086ff8539b971a
Philip Doxakis <doxakis@users.noreply.github.com>
2017-09-28 00:34:28 -0400

    feat: perceptron network with iris dataset

---
+++ PerceptronNeuralNetwork/PerceptronNetwork.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronNeuralNetwork
+{
+	public class PerceptronNetwork
+	{
+		public int InputSize { get; set; }
+		public int HiddenSize { get; set; }
+		public int OutputSize { get; set; }
+		public double LearnRate { get; set; }
+		public double Momentum { get; set; }
+
+		// Hidden layer
+		public double[] HiddenBias { get; set; }
+		public double[] HiddenBiasDelta { get; set; }
+		public double[] HiddenGradient { get; set; }
+		public double[] HiddenValue { get; set; }
+		public double[] HiddenWeight { get; set; }
+		public double[] HiddenWeightDelta { get; set; }
+
+		// Output layer
+		public double[] OutputBias { get; set; }
+		public double[] OutputBiasDelta { get; set; }
+		public double[] OutputGradient { get; set; }
+		public double[] OutputValue { get; set; }
+		public double[] OutputWeight { get; set; }
+		public double[] OutputWeightDelta { get; set; }
+
+		public PerceptronNetwork(
+			int inputSize,
+			int hiddenSize,
+			int outputSize,
+			double learnRate,
+			double momentum)
+		{
+			InputSize = inputSize;
+			HiddenSize = hiddenSize;
+			OutputSize = outputSize;
+			LearnRate = learnRate;
+			Momentum = momentum;
+
+			HiddenBias = new double[hiddenSize];
+			for (int i = 0; i < HiddenBias.Length; i++)
+			{
+				HiddenBias[i] = GetRandom();
+			}
+			HiddenBiasDelta = new double[hiddenSize];
+			HiddenGradient = new double[hiddenSize];
+			HiddenValue = new double[hiddenSize];
+			HiddenWeight = new double[hiddenSize * inputSize];
+			HiddenWeightDelta = new double[hiddenSize * inputSize];
+			for (int i = 0; i < HiddenWeight.Length; i++)
+			{
+				HiddenWeight[i] = GetRandom();
+			}
+
+			OutputBias = new double[outputSize];
+			for (int i = 0; i < OutputBias.Length; i++)
+			{
+				OutputBias[i] = GetRandom();
+			}
+			OutputBiasDelta = new double[outputSize];
+			OutputGradient = new double[outputSize];
+			OutputValue = new double[outputSize];
+			OutputWeight = new double[outputSize * hiddenSize];
+			OutputWeightDelta = new double[outputSize * hiddenSize];
+			for (int i = 0; i < OutputWeight.Length; i++)
+			{
+				OutputWeight[i] = GetRandom();
+			}
+		}
+
+		public void Train(double[][] values, double[][] targets, int numEpochs)
+		{
+			if (values.Length != targets.Length)
+			{
+				throw new ArgumentException("Values and targets are not the same length.");
+			}
+
+			for (var i = 0; i < numEpochs; i++)
+			{
+				for (int j = 0; j < values.Length; j++)
+				{
+					ForwardPropagate(values[j]);
+					BackPropagate(values[j], targets[j]);
+				}
+			}
+		}
+
+		private void ForwardPropagate(double[] inputs)
+		{
+			// Hidden layer.
+			for (int i = 0; i < HiddenSize; i++)
+			{
+				var x = HiddenBias[i];
+				for (int j = 0; j < InputSize; j++)
+				{
+					int pos = j + i * InputSize;
+					x += HiddenWeight[pos] * inputs[j];
+				}
+				HiddenValue[i] = SigmoidOutput(x);
+			}
+
+			// Output layer.
+			for (int i = 0; i < OutputSize; i++)
+			{
+				var x = OutputBias[i];
+				for (int j = 0; j < HiddenSize; j++)
+				{
+					int pos = j + i * HiddenSize;
+					x += OutputWeight[pos] * HiddenValue[j];
+				}
+				OutputValue[i] = SigmoidOutput(x);
+			}
+		}
+
+		private void BackPropagate(double[] inputs, double[] targets)
+		{
+			// Calculate gradient (output layer)
+			for (int i = 0; i < OutputSize; i++)
+			{
+				OutputGradient[i] = (targets[i] - OutputValue[i]) * SigmoidDerivative(OutputValue[i]);
+			}
+
+			// Calculate gradient (hidden layer)
+			for (int i = 0; i < HiddenSize; i++)
+			{
+				double sum = 0;
+				for (int j = 0; j < OutputSize; j++)
+				{
+					sum += OutputGradient[j] * OutputWeight[i + j * HiddenSize];
+				}
+				HiddenGradient[i] = sum * SigmoidDerivative(HiddenValue[i]);
+			}
+
+			// Update weights (hidden layer)
+			for (int i = 0; i < HiddenSize; i++)
+			{
+				var prevDelta = HiddenBiasDelta[i];
+				HiddenBiasDelta[i] = LearnRate * HiddenGradient[i];
+				HiddenBias[i] += HiddenBiasDelta[i] + Momentum * prevDelta;
+
+				for (int j = 0; j < InputSize; j++)
+				{
+					int pos = j + i * InputSize;
+					prevDelta = HiddenWeightDelta[pos];
+
+					HiddenWeightDelta[pos] = LearnRate * HiddenGradient[i] * inputs[j];
+
+					HiddenWeight[pos] += HiddenWeightDelta[pos] + Momentum * prevDelta;
+				}
+			}
+
+			// Update weights (output layer)
+			for (int i = 0; i < OutputSize; i++)
+			{
+				var prevDelta = OutputBiasDelta[i];
+				OutputBiasDelta[i] = LearnRate * OutputGradient[i];
+				OutputBias[i] += OutputBiasDelta[i] + Momentum * prevDelta;
+
+				for (int j = 0; j < HiddenSize; j++)
+				{
+					int pos = j + i * HiddenSize;
+					prevDelta = OutputWeightDelta[pos];
+					OutputWeightDelta[pos] = LearnRate * OutputGradient[i] * HiddenValue[j];
+					OutputWeight[pos] += OutputWeightDelta[pos] + Momentum * prevDelta;
+				}
+			}
+		}
+
+		public double[] Compute(double[] inputs)
+		{
+			ForwardPropagate(inputs);
+			return OutputValue;
+		}
+
+		public double CalculateError(params double[] targets)
+		{
+			double error = 0;
+			for (int i = 0; i < OutputSize; i++)
+			{
+				error += Math.Abs(targets[i] - OutputValue[i]);
+			}
+			return error;
+		}
+
+		public static double SigmoidOutput(double x)
+		{
+			return x < -45.0 ? 0.0 : x > 45.0 ? 1.0 : 1.0 / (1.0 + Math.Exp(-x));
+		}
+
+		public static double SigmoidDerivative(double x)
+		{
+			return x * (1 - x);
+		}
+
+		private static readonly Random Random = new Random();
+
+		public static double GetRandom()
+		{
+			return 2 * Random.NextDouble() - 1;
+		}
+	}
+}
--- PerceptronNeuralNetwork/Program.cs
+++ PerceptronNeuralNetwork/Program.cs
@@ -1,15 +1,199 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PerceptronNeuralNetwork
 {
+	public class DataSet
+	{
+		public double[] Values { get; set; }
+		public double[] Targets { get; set; }
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			
+			// Csv file.
+			var fileName = "iris.csv";
+
+			// sepal_length,sepal_width,petal_length,petal_width
+			int numberOfValues = 4; // Fields in csv file.
+
+			// is_setosa,is_versicolor,is_virginica (can be: 0 = false or 1 = true)
+			int numberOfTargets = 3; // Fields in csv file.
+
+			PerceptronNetwork network = new PerceptronNetwork(
+				inputSize: numberOfValues,
+				hiddenSize: 3,
+				outputSize: numberOfTargets,
+				learnRate: 0.4,
+				momentum: 0.95);
+
+			List<DataSet> fullDataset = LoadCsv(fileName, numberOfValues, numberOfTargets);
+			Shuffle(fullDataset);
+
+			int validationProportion = 15; // %
+			int crossValidationProportion = 15; // %
+
+			List<DataSet> validationDataSet = fullDataset
+				.Take(fullDataset.Count * validationProportion / 100)
+				.ToList();
+
+			List<DataSet> crossValidationDataSet = fullDataset
+				.Skip(fullDataset.Count * validationProportion / 100)
+				.Take(fullDataset.Count * crossValidationProportion / 100)
+				.ToList();
+
+			List<DataSet> trainDataset = fullDataset
+				.Skip(fullDataset.Count *
+					(100 - validationProportion - crossValidationProportion) / 100)
+				.ToList();
+
+			Console.WriteLine("Training started...\n");
+
+			int iterations = 10;
+
+			for (int i = 0; i < iterations; i++)
+			{
+				network.Train(
+					values: trainDataset.Select(m => m.Values).ToArray(),
+					targets: trainDataset.Select(m => m.Targets).ToArray(),
+					numEpochs: 100);
+
+				var errors = new List<double>();
+				foreach (var dataSet in trainDataset)
+				{
+					network.Compute(dataSet.Values);
+					errors.Add(network.CalculateError(dataSet.Targets));
+				}
+				var error = errors.Average();
+				Console.WriteLine("Iteration #" + (i + 1) + ", cost = " + error);
+			}
+
+			Console.WriteLine("\nTraining completed.\n");
+
+			Console.WriteLine("Testing network with train dataset:\n");
+			TestNetwork(network, trainDataset);
+
+			Console.WriteLine("Testing network with validation dataset:\n");
+			TestNetwork(network, validationDataSet);
+
+			Console.WriteLine("Testing network with cross validation dataset:\n");
+			TestNetwork(network, crossValidationDataSet);
+
+			stopwatch.Stop();
+			Console.WriteLine("Duration: " + stopwatch.Elapsed);
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadLine();
+		}
+
+		private static void TestNetwork(
+			PerceptronNetwork network,
+			List<DataSet> validationDataSet)
+		{
+			int nbGood = 0;
+			int nbBad = 0;
+
+			foreach (var dataSet in validationDataSet)
+			{
+				Console.Write("Input: " +
+					string.Join(" ", dataSet.Values
+						.Select(m => string.Format("{0:0.0}", m))));
+
+				var result = network.Compute(dataSet.Values.ToArray());
+
+				Console.Write(" Expected: " +
+					string.Join(" ", dataSet.Targets
+						.Select(m => string.Format("{0:0.0}", m))));
+
+				Console.Write(" Computed: " +
+					string.Join(" ", result
+						.Select(m => string.Format("{0:0.0}", m))));
+
+				int indexMaxExpected = !dataSet.Values.Any()
+					? -1
+					: dataSet.Targets
+						.Select((value, index) => new { Value = value, Index = index })
+						.Aggregate((a, b) => (a.Value > b.Value) ? a : b)
+						.Index;
+
+				int indexMaxComputed = !result.Any()
+					? -1
+					: result
+						.Select((value, index) => new { Value = value, Index = index })
+						.Aggregate((a, b) => (a.Value > b.Value) ? a : b)
+						.Index;
+
+				if (indexMaxComputed == indexMaxExpected)
+				{
+					Console.WriteLine(" GOOD RESULT");
+					nbGood++;
+				}
+				else
+				{
+					Console.WriteLine(" BAD RESULT");
+					nbBad++;
+				}
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("# Good : " + nbGood);
+			Console.WriteLine("# Bad  : " + nbBad);
+			Console.WriteLine("% Good : " +
+				string.Format("{0:0.0}", 100.0 * nbGood / (nbGood + nbBad)));
+			Console.WriteLine();
+		}
+
+		private static List<DataSet> LoadCsv(
+			string fileName,
+			int numberOfValues,
+			int numberOfTargets)
+		{
+			List<DataSet> myDataSet = new List<DataSet>();
+			
+			var lines = File.ReadLines(fileName)
+				.Skip(1) /* Skip header. */;
+			
+			foreach (var line in lines)
+			{
+				var values = line.Split(',')
+					.Take(numberOfValues)
+					.Select(m => double.Parse(m, CultureInfo.InvariantCulture))
+					.ToArray();
+
+				var targets = line.Split(',')
+					.Skip(numberOfValues)
+					.Take(numberOfTargets)
+					.Select(m => double.Parse(m, CultureInfo.InvariantCulture))
+					.ToArray();
+
+				myDataSet.Add(new DataSet { Values = values, Targets = targets });
+			}
+			
+			return myDataSet;
+		}
+
+		public static void Shuffle<E>(IList<E> list)
+		{
+			Random rng = new Random();
+			int n = list.Count;
+			while (n > 1)
+			{
+				n--;
+				int k = rng.Next(n + 1);
+				E value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
 		}
 	}
 }
